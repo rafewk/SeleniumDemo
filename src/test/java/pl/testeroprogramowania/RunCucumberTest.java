3e17fa68a41e6039f8fa3c7546c113b85e45af72
rafewk <97rafal97@gmail.com>
2022-01-29 14:48:29 +0100

    add html and json reports

--- src/test/java/pl/testeroprogramowania/RunCucumberTest.java
+++ src/test/java/pl/testeroprogramowania/RunCucumberTest.java
@@ -5,6 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features")
+@CucumberOptions(features = "src/test/resources/features",
+        plugin={"pretty","html:target/index.html","json:target/cucumber.json"})
 public class RunCucumberTest {
 }
